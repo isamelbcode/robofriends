aa147874a4c9c6f1090a46384f7be83b63955450
isamelb <isamelb@gmail.com>
2020-06-13 14:20:49 +0100

    forcing changes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import thunkMiddleware from 'redux-thunk';
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./containers/App";
-import registerServiceWorker from "./registerServiceWorker";
+import * as serviceWorker from './serviceWorker';
 import "tachyons";
 import { searchRobots, requestRobots} from "./reducers";
 const logger = createLogger();
@@ -21,4 +21,7 @@ ReactDOM.render(
 
   document.getElementById("root")
 );
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
